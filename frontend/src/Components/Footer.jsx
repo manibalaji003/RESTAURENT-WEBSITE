a6eeaf70524ac43489ab62003c7d2452dd60ec4b
manibalaji003 <manibalaji22003@gmail.com>
2024-06-25 19:38:29 +0530

    git branch commit

---
+++ frontend/src/Components/Footer.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='footer'>
+            <div className='logo d-flex justify-content-center'>
+                <div><i className="socialmedia bi bi-instagram p-4"></i></div>
+                <div><i className="socialmedia bi bi-youtube p-4"></i></div>
+                <div><i className="socialmedia bi bi-facebook p-4 "></i></div>
+              
+            </div>
+            <hr />
+            <center>  Made with &#x2764;  by Mani balaji & raafih ahmed</center><br />
+    </div>
+  )
+}
+
+export default Footer
---
+++ frontend/src/Components/Special.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import { Col, Container, Image, Row } from 'react-bootstrap'
+import poster1 from '../assets/poster1.png'
+import poster2 from '../assets/poster2.png'
+import poster3 from '../assets/poster3.png'
+import poster4 from '../assets/poster4.png'
+import poster5 from '../assets/poster5.png'
+import poster6 from '../assets/poster6.png'
+const Special = () => {
+  return (
+    <div >
+        <Container className='d-flex justify-content-center'>
+        <Row >
+            <Col>
+                <Image src={poster1}  className='image m-5' alt='poster-1'/>
+            </Col>
+            <Col>
+                <Image src={poster2} className='image m-5' />
+            </Col>
+            <Col>
+            <Image src={poster3} className='image  m-5' />
+            </Col>
+        </Row>
+        </Container>
+        <Container className='d-flex justify-content-center'>
+        <Row >
+        <Col>
+                <Image src={poster4}  className='image m-5' alt='poster-1'/>
+            </Col>
+            <Col>
+                <Image src={poster5} className='image m-5' />
+            </Col>
+            <Col>
+            <Image src={poster6} className='image m-5' />
+            </Col>
+        </Row>
+        </Container>
+    </div>
+  )
+}
+
+export default Special
