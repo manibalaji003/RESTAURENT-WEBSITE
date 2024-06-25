ce1874c1ecdb501d2ecb0a88d56d9a46fe7e867d
Raafi2003 <sraafih810@gmail.com>
2024-06-25 13:07:58 +0530

    Completed food items schema

---
+++ backend/index.js
@@ -0,0 +1,61 @@
+const express = require('express')
+const morgan = require('morgan')
+const mongoose = require('mongoose')
+const cors = require('cors')
+require('dotenv/config');
+
+//schema
+const menuSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    image: String,
+    description: String,
+    price: Number
+})
+
+//models
+const Menu = mongoose.model('items', menuSchema);
+
+//middleware
+const app = express();
+app.use(express.json());
+app.use(morgan('tiny'))
+
+
+const PORT = process.env.PORT;
+
+//routes
+app.get('/post', (req,res)=>{
+    res.send("nothing!")
+})
+app.post('/post', (req,res)=>{
+    const menu = new Menu({
+        name: req.body.name,
+        image: req.body.image,
+        description: req.body.description,
+        price: req.body.price
+    })
+    menu.save().then((created)=>{
+        res.status(201).json(created)
+    }).catch((err)=>{
+        res.status(500).json({
+            error: err,
+            success: false
+        })
+    })
+})
+
+//database
+mongoose.connect(process.env.CONNECTION_STRING)
+.then(()=>{
+    console.log('Database connected....')
+}).catch((err)=>{
+    console.log(err);
+})
+
+//server
+app.listen(PORT, ()=>{
+    console.log(`App listening in port: ${PORT}`);
+})
