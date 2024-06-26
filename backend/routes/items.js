2a4d715e10016c25b2bc85c7b56a8d9dcff74485
Raafi2003 <sraafih810@gmail.com>
2024-06-26 13:31:09 +0530

    updated delete and update in items collection

--- backend/routes/items.js
+++ backend/routes/items.js
@@ -2,14 +2,46 @@ const {Item} = require('../models/items')
 const express = require('express')
 const router = express.Router();
 
-router.get('/', async (req,res)=>{
-    const productList = await Item.find();
-
-    if(!productList) {
-        res.status(500).json({success: false})
-    } 
-    res.send(productList);
+// get all items
+router.get(`/`, async (req,res)=>{ 
+    Item.find().then((obj)=>{
+        res.status(200).json(obj)
+    }).catch((err)=>{
+        res.status(500).json({
+            error: err,
+            success: false
+        }) 
+    })
 })
+
+// get specific item by id
+router.get(`/:id`, async (req,res)=>{
+    Item.findById(req.params.id).then((obj)=>{
+        res.status(200).json(obj)
+    }).catch((err)=>{
+        res.status(500).json({
+            error: err,
+            success: false
+        })
+    })
+})
+
+// delete by id
+router.delete('/:id', (req,res)=>{
+    console.log(Item)
+    Item.findByIdAndDelete(req.params.id).then((obj)=>{
+        console.log('deleted successfully!')
+        res.status(200).json(obj)
+    }).catch((err)=>{
+        console.log(err);
+        res.status(400).json({
+            error: err,
+            success: false
+        })
+    })
+})
+
+// post item
 router.post('/', (req,res)=>{
     const item = new Item({
         name: req.body.name,
@@ -20,6 +52,24 @@ router.post('/', (req,res)=>{
     })
     item.save().then((created)=>{
         res.status(201).json(created)
+    }).catch((err)=>{ 
+        res.status(500).json({
+            error: err,
+            success: false
+        })
+    })
+})
+
+// update item by id
+router.put('/:id',(req,res)=>{
+    Item.findByIdAndUpdate(req.params.id, {
+        name: req.body.name,
+        category: req.body.category,
+        image: req.body.image,
+        description: req.body.description,
+        price: req.body.price
+    }).then((obj)=>{
+        res.status(201).json(obj)
     }).catch((err)=>{
         res.status(500).json({
             error: err,
