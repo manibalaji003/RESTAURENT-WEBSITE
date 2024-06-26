9439edc99c8305a18a3d0d394d70802f6d985fb4
manibalaji003 <manibalaji22003@gmail.com>
2024-06-26 15:26:32 +0530

    axios commit

---
+++ frontend/src/Pages/Menu.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import axios from 'axios'
+
+const Menu = () => {
+
+    let response=axios.get(''); 'pathe name kudu inga '
+    console.log(response.data);
+
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default Menu
