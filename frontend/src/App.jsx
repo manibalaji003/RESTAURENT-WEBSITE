ca1f9d5c6db6f771711bfed5cb96f3716282546a
manibalaji003 <manibalaji22003@gmail.com>
2024-06-27 14:07:26 +0530

    menu page cmmt

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,15 +2,18 @@ import { useState } from 'react'
 import './App.css'
 import Home from './Pages/Home'
 import Menu from './Pages/Menu'
+import {Routes,Router, BrowserRouter, Route} from 'react-router-dom'
 
 function App() {
   
 
   return (
     <>
-
-    <Home />
-    <Menu />
+        
+        <Menu />
+    
+    
+    
   </>
   )
 }
--- frontend/src/Pages/Menu.jsx
+++ frontend/src/Pages/Menu.jsx
@@ -1,18 +1,141 @@
-import React from 'react'
-import axios from 'axios'
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import { Container, Card,Button } from 'react-bootstrap';
+const StarRating = ({ count }) => {
+  const stars = Array.from({ length: count }, (_, index) => (
+    <i key={index} className="bi bi-star-fill" style={{color:'rgb(218,165,32)'}}></i>
+  ));
+  return <span>{stars}</span>;
+};
 
 const Menu = () => {
+  const [itemData, setitemData] = useState({});
+  const [loading, setLoading] = useState(true);
 
-  axios.get('http://localhost:3300/api/v1/items/Kheer').then((obj)=>{
-    console.log(obj.data)
-  }).catch((err)=>{
-    console.log(err)
-  })
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get('http://localhost:3300/api/v1/items/cat');
+        
+          setitemData(response.data);
+      
+          
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchData();
+  }, []);
+
+  useEffect(() => {
+    console.log('itemData:', itemData);
+  }, [itemData]);
+
+  
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <>
+    {/* <Desserts desseretsdata={itemData.Desserts} />
+    <Beverages  bevarage={itemData.Beverages}/>
+     */}
+     </>
+  );
+};
+
+
+
+export const Beverages= (props) => {
+  
+  let beveragesresponsedata=props.bevarage;
+
   return (
     <div>
+        <h3>Desserts</h3>
+      <div className='dessserts'>
+      {beveragesresponsedata.length > 0 ? (
+        beveragesresponsedata.map(({ name, rating, price, image, description }) => (
+          <div key={name}>
+            <Card style={{ width: '18rem', margin: '1rem' }}>
+              <Card.Img variant="top" src={image}  width={150} height={150}/>
+              <Card.Body>
+                <Card.Title>{name}</Card.Title>
+                <Card.Text>
+                  Description: {description}
+                </Card.Text>
+              
+                <Card.Subtitle>
+                  Rating: <StarRating count={rating} />
+                </Card.Subtitle>
+                <Container className='d-flex justify-content-between'><b className='pricetag '>&#8377; {price}</b><Button>Add to cart</Button></Container>
+                    
+                   
+               
+              </Card.Body>
+            </Card>
+          </div>
+        ))
+      ) : (
+        <div>No items available</div>
+      )}
+  
+      </div>
 
     </div>
   )
 }
 
-export default Menu
+
+
+
+
+export const Desserts = (props) => {
+
+  let dessertData=props.desseretsdata;
+
+  return (
+    <div >
+      <h3>Desserts</h3>
+      <div className='dessserts'>
+      {dessertData.length > 0 ? (
+        dessertData.map(({ name, rating, price, image, description }) => (
+          <div key={name}>
+            <Card style={{ width: '18rem', margin: '1rem' }}>
+              <Card.Img variant="top" src={image}  width={150} height={150}/>
+              <Card.Body>
+                <Card.Title>{name}</Card.Title>
+                <Card.Text>
+                  Description: {description}
+                </Card.Text>
+              
+                <Card.Subtitle>
+                  Rating: <StarRating count={rating} />
+                </Card.Subtitle>
+                <Container className='d-flex justify-content-between'><b className='pricetag '>&#8377; {price}</b><Button>Add to cart</Button></Container>
+                    
+                   
+               
+              </Card.Body>
+            </Card>
+          </div>
+        ))
+      ) : (
+        <div>No items available</div>
+      )}
+  
+      </div>
+    </div>
+  )
+}
+
+
+
+
+
+
+export default Menu;
