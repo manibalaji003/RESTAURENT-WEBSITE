886600232c312b7d32dcd49bdf9935ad00351f8f
manibalaji003 <manibalaji22003@gmail.com>
2024-07-15 13:01:12 +0530

    cart commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,20 +5,25 @@ import Menu from './Pages/Menu'
 import {Routes,Router, BrowserRouter, Route} from 'react-router-dom'
 import LoginPage from './Pages/LoginPage'
 import Signup from './Pages/Signup'
+import Cart from './Pages/Cart'
+import Header from './Components/Header'
 
 function App() {
   
+const [cart, setCart]=useState([ ]);
+
 
   return (
     <>
         
         
 <BrowserRouter>
-        
+        <Header />
         <Routes>
-            <Route path='/' element={<Home />} />
+            <Route path='/' element={<Home   />} />
             <Route path='/loginpage' element={<LoginPage />} />
-            <Route path='/menu' element={<Menu />} />
+            <Route path='/menu' element={<Menu  cart={cart} setCart={setCart} />} />
+            <Route path='cart' element={<Cart  cart={cart}  />} />
             <Route path='/signup' element={<Signup />} />
             
           </Routes>
