af83972901e82cf34d14c0563be617ad08996940
manibalaji003 <manibalaji22003@gmail.com>
2024-07-15 20:43:34 +0530

    cart commit commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,37 +1,28 @@
-import { useState } from 'react'
-import './App.css'
-import Home from './Pages/Home'
-import Menu from './Pages/Menu'
-import {Routes,Router, BrowserRouter, Route} from 'react-router-dom'
-import LoginPage from './Pages/LoginPage'
-import Signup from './Pages/Signup'
-import Cart from './Pages/Cart'
-import Header from './Components/Header'
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './Pages/Home';
+import Menu from './Pages/Menu';
+import LoginPage from './Pages/LoginPage';
+import Signup from './Pages/Signup';
+import Cart from './Pages/Cart';
+import Header from './Components/Header';
+import './App.css';
 
 function App() {
-  
-const [cart, setCart]=useState([ ]);
-
+  const [cart, setCart] = useState([]);
 
   return (
-    <>
-        
-        
-<BrowserRouter>
-        <Header />
-        <Routes>
-            <Route path='/' element={<Home   />} />
-            <Route path='/loginpage' element={<LoginPage />} />
-            <Route path='/menu' element={<Menu  cart={cart} setCart={setCart} />} />
-            <Route path='cart' element={<Cart  cart={cart}  />} />
-            <Route path='/signup' element={<Signup />} />
-            
-          </Routes>
-          </BrowserRouter>   
-    
-    
-  </>
-  )
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/loginpage" element={<LoginPage />} />
+        <Route path="/menu" element={<Menu cart={cart} setCart={setCart} />} />
+        <Route path="/cart" element={<Cart cart={cart} />} />
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
