579f39dd7eddc7e137b14de5ba8c7719e5cefac3
manibalaji003 <manibalaji22003@gmail.com>
2024-08-22 16:30:41 +0530

    adminpage view commit

--- frontend/src/Pages/AdminDashboard.jsx
+++ frontend/src/Pages/AdminDashboard.jsx
@@ -1,4 +1,4 @@
-import { Container, Form, Button } from 'react-bootstrap';
+import { Container, Form, Button,Row} from 'react-bootstrap';
 import { useEffect, useState } from 'react';
 import axios from 'axios';
 
@@ -27,16 +27,23 @@ const AdminDashboard = () => {
 
 export default AdminDashboard;
 
+
+
 const AddProducts = (props) => {
   const data = props.admindata1;
   const [categoryitem, setCategory] = useState(Object.keys(data)[0] || '');
-  const [item,setitem]=useState()
+  const [selectedItem, setSelectedItem] = useState(null);
+  const [disable,setdisable]=useState(false);
 
   const dropdown = (e) => {
     setCategory(e.target.value);
+    setSelectedItem(null); // Reset selected item when category changes
   };
+
   const dropdown2 = (e) => {
-   
+    const selectedItemName = e.target.value;
+    const selectedItemData = data[categoryitem].find(item => item.name === selectedItemName);
+    setSelectedItem(selectedItemData);
   };
 
   return (
@@ -46,7 +53,7 @@ const AddProducts = (props) => {
           <Form.Group>
             <Form.Label>Category</Form.Label>
             <Form.Select value={categoryitem} onChange={dropdown}>
-            <option>Select</option>
+              <option>Select</option>
               {Object.keys(data).map((item) => (
                 <option key={item} value={item}>
                   {item}
@@ -60,11 +67,11 @@ const AddProducts = (props) => {
         <Form>
           <Form.Group>
             <Form.Label>Food Name</Form.Label>
-            <Form.Select placeholder='- - select- -' onchange={dropdown2}>
-              
+            <Form.Select value={selectedItem?.name || ''} onChange={dropdown2}>
+              <option>Select</option>
               {data[categoryitem] &&
                 data[categoryitem].map((item) => (
-                  <option key={item._id} value={item._id}>
+                  <option key={item._id} value={item.name}>
                     {item.name}
                   </option>
                 ))}
@@ -72,9 +79,22 @@ const AddProducts = (props) => {
           </Form.Group>
         </Form>
       </Container>
-      <Container style={{backgroundColor:'grey'}}>
-                
-      </Container>
+      {selectedItem && (
+        <Container style={{ backgroundColor: 'grey', padding: '10px', marginTop: '10px' }}>
+         
+          <img src={selectedItem.image} alt={selectedItem.name} style={{ width: '100px', height: '100px' }} />
+          <Form>
+              <Form.Group as={Row}>
+                  <Form.Label>Name</Form.Label>
+                  <Form.Control type="text" value={selectedItem.name} disabled={disable}/>
+              </Form.Group>
+              <Form.Group as={Row}>
+                  <Form.Label>description</Form.Label>
+                  <Form.Control type="text" value={selectedItem.description} disabled={disable}/>
+              </Form.Group>
+          </Form>
+        </Container>
+      )}
     </div>
   );
 };
